860d5727ead5b7f5ab194823b36feca5e7b63f97
deepakinnoblitz <deepak.innoblitz@gmail.com>
2026-07-29 05:27:55 +0000

    Changes in Document Templete

--- src/pages/hr-document-templates/new.tsx
+++ src/pages/hr-document-templates/new.tsx
@@ -1,4 +1,5 @@
 import { CONFIG } from 'src/config-global';
+
 import { HRDocumentTemplateCreateView } from 'src/sections/hr-document-templates/view';
 
 export default function HRDocumentTemplateCreatePage() {
