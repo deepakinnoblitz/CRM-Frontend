8d69d26e50f2e80d284697b5daa5035305d9a1f0
deepakinnoblitz <deepak.innoblitz@gmail.com>
2026-09-11 06:04:22 +0000

    Changes in Task Detail and Manager Add Issue

--- src/sections/task-manager/view/task-manager-view.tsx
+++ src/sections/task-manager/view/task-manager-view.tsx
@@ -298,7 +298,7 @@ export default function TaskManagerView() {
                             </ToggleButton>
                         </ToggleButtonGroup>
 
-                        {canSeeAll || canCreateTask && (
+                        {(canSeeAll || canCreateTask) && (
                             <Button
                                 variant="contained"
                                 startIcon={<Iconify icon="mingcute:add-line" />}
